ec08bf249af65b7e0a340794d8100f5d3f4759cd
tohzen <melvin.l.mobley@gmail.com>
2021-04-04 20:25:58 -0400

    done

--- main.js
+++ main.js
@@ -1,3 +1,123 @@
+function addToDo(obj) {
+  todos.push(obj)
+}
+
+function remove(index) { 
+  todos.splice(index,1)
+  
+}
+
+let global_counter = 6;
+const button = document.querySelector(".add-todo");
+
+const addList = document.querySelector(".todo-list");
+const item = document.querySelectorAll(".todo-item")
+
+function IdTag() {
+  global_counter++;
+}
+
+let mouseDown = false;
+
+
+function makeItem() {
+  const newInput = document.querySelector(".todo-input");
+  if(newInput.value === ""){
+    return alert("Please input text")
+  }else{ 
+  const newItem = {
+    text: newInput.value,
+    complete: false,
+    priority: 2,
+    id: global_counter,
+  }
+
+  IdTag(newItem)
+  addToDo(newItem)
+  printTodo(newItem)
+  newInput.value = ""
+  console.log(todos)
+}
+}
+button.addEventListener("click",makeItem);
+
+
+
+const printTodo = function(obj) {
+  let item = document.createElement("li");
+  item.innerText = obj.text
+  item.classList.add("todo-item")
+  item.id = obj.id;
+  if(obj.complete == true) {
+    item.classList.add("complete")
+  }
+  const addList = document.querySelector(".todo-list");
+  addList.appendChild(item)
+  console.log(item)
+  
+  
+}
+
+
+function done() {
+  let item =  document.querySelector(".todo-item").value
+    let list = document.querySelectorAll(".todo-list")
+  for(item of list)
+        item.classList.add("complete")
+           
+}
+  
+  console.log("shit")
+
+
+
+
+addList.addEventListener("click",done)
+
+
+function readList(todo) {
+  if(todo == undefined){
+    return ""
+  }else{
+    for(todo of todos) {
+      printTodo(todo)
+      
+      console.log()
+    }
+
+  }
+    
+}
+
+readList(todos);
+
+const clearButton = document.querySelector(".todo-clear")
+function clearAll(thing) {
+  const items = document.querySelectorAll(".todo-item")
+  for(thing of items) {
+    thing.remove();
+    todos.pop(todos)
+  }
+}
+clearButton.addEventListener("click", refresh);
+
+
+function refresh(obj) {
+  clearAll(todos);
+  readList(todos);
+  console.log(todos)
+}
+
+
+
+
+
+
+// A function that, given a todo object, adds an item to our todo list array.
+// A function that removes an item at a given index from our todo list array. You can use splice!
+// A function that takes in a todo object and displays it on the DOM. This is a pretty big function, so we'll walk through the different parts of it.
+// =============================================================================
+
 // =============================================================================
 
 // Remember that our list is called `todos`, and it's in `todos.js`. It's not in this file, nor is it being `require`-d in, but it's available globally because its file is loaded in BEFORE this one in `index.html`.
@@ -11,58 +131,38 @@
 
 // And now: code away!
 
-// =============================================================================
-
-// A function that, given a todo object, adds an item to our todo list array.
 
 
 
-// A function that removes an item at a given index from our todo list array. You can use splice!
+// 
 
-
-
-// A function that takes in a todo object and displays it on the DOM. This is a pretty big function, so we'll walk through the different parts of it.
-const printTodo = function(todo) {
-  // Use `document.createElement` to make an <li>.
-
-  
-  // Set its text (preferably using `.innerText`) to be our given object's text field. Check out what a todo object looks like in `todos.js` if you need to!
-
-
-  // Give our new li a `todo-item` class using `classList`. This will allow us to style it later if we want.
-
-
-  // Give our new li an id that is the object's id. This is so that we have a matching relationship between todo _html elements_ and their corresponding _array objects_. Now we'll be able to find the corresponding array object when they click to toggle the completeness on a DOM element.
-
-  
-  // Give the li a `complete` class if the todo object indicates it was complete already. (Again, check the `todos.js` to see what the objects look like!)
-
-
-  // Query the todo list <ol> and store it in a variable
-
-
-  // Append the li we made to the ol as the last child using `.appendChild`. If this isn't working for you, check what is being appended to what!
-
-}
-
-
-// A function that print ALL todos. It should loop through our todos array and call the above print-one-todo function on each one.
-
-
+// 
 
 // Now here in the global code, call the above function, so our todos array gets printed out on page load (which is when global code is run). This is the only time we're calling a function ourselves; the rest is event listeners and helper functions that run when the user interacts with the DOM!
-
-
 
 // A function that clears all todos from the DOM. This is a great helper function for refreshing our todos.
 // Test it in the console and see if your list disappears!
 
-
-
 // A function that refreshes our page by calling each of the two above functions. Since printing all todos onto the DOM is based on our todos array, if we make a change to our todos array, we can simply call this function, which will make our DOM match our todos array by simply clearing the page and repopulating it according to our todos' new state.
 
+// Use `document.createElement` to make an <li>.
 
 
+// Set its text (preferably using `.innerText`) to be our given object's text field. Check out what a todo object looks like in `todos.js` if you need to!
+
+
+// Give our new li a `todo-item` class using `classList`. This will allow us to style it later if we want.
+
+
+// Give our new li an id that is the object's id. This is so that we have a matching relationship between todo _html elements_ and their corresponding _array objects_. Now we'll be able to find the corresponding array object when they click to toggle the completeness on a DOM element.
+
+// Give the li a `complete` class if the todo object indicates it was complete already. (Again, check the `todos.js` to see what the objects look like!)
+
+// Query the todo list <ol> and store it in a variable
+
+// Append the li we made to the ol as the last child using `.appendChild`. If this isn't working for you, check what is being appended to what!
+
+// A function that print ALL todos. It should loop through our todos array and call the above print-one-todo function on each one.
 /*
 Let's wire it all together. Add an event listener for the add todo button that will:
 1. Queries the input box. We will need that node element again, so save it to a variable!
@@ -75,7 +175,7 @@ Let's wire it all together. Add an event listener for the add todo button that w
 
 
 /* 
- Run over to the HTML and add a button for CLEAR TODOS or REMOVE TODOS or some such, giving it a class or id of your choice. Now let's wire up that button, giving it a click event listener that clears all todos from the DOM (we have a function for that!) and removes all todo objects from the todos array as well.
+Run over to the HTML and add a button for CLEAR TODOS or REMOVE TODOS or some such, giving it a class or id of your choice. Now let's wire up that button, giving it a click event listener that clears all todos from the DOM (we have a function for that!) and removes all todo objects from the todos array as well.
 */
 
 
